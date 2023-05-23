4c747219b590a62a2e9207d048765c0f70d5a7db
Nguyen Khac Lam <khaclam2409@gmail.com>
2023-05-23 17:19:18 +0700

    Update mongodb url

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/auth'),
+    MongooseModule.forRoot('mongodb://auth-mongo-service:27017/auth'),
     UserModule,
   ],
   controllers: [],
