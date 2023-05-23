0be3c4713b38cf35edbe12d70d7222da9b2143c1
Nguyen Khac Lam <khaclam2409@gmail.com>
2023-05-23 16:10:14 +0700

    Add mongo

--- auth/src/app.module.ts
+++ auth/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://auth-mongo-service:27017/auth_db'),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
